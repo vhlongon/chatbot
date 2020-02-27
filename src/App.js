dd30f72a1ee293a50ede52e66a323d567c6b206e
Victor Hugo Longon <victor.longon@etraveli.com>
2020-02-27 17:58:32 +0100

    add react-chat-wdiget

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Widget, addResponseMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import logo from "./logo.svg";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    addResponseMessage("I welcome here!");
+  }, []);
+  const handleNewUserMessage = newMessage => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+    // addResponseMessage(response);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={logo}
+        title="My awesome chatbot"
+        subtitle="V & S limited"
+      />
     </div>
   );
-}
+};
 
 export default App;
